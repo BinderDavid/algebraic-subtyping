8acb3c04624e32533d85f30d27a8050874926a83
BinderDavid <dbinder1989@googlemail.com>
2020-07-26 17:25:32 +0200

    Add Syntax for Terms and Simple Types

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,1 +1,29 @@
 module Syntax where
+
+
+type PrimName = String
+type VarName = String
+type Label = String
+
+data IsRec = Rec | NonRec
+
+data Term
+  = TmLit Int
+  | TmVar VarName
+  | TmLam VarName Term
+  | TmApp Term Term
+  | TmRcd [(Label, Term)]
+  | TmSel Term Label
+  | TmLet IsRec VarName Term Term
+
+
+data VariableState = MkVariableState
+  { lowerBounds :: [SimpleType]
+  , upperBounds :: [SimpleType]
+  }
+
+data SimpleType
+  = TyVar VariableState
+  | TyPrim PrimName
+  | TyFun SimpleType SimpleType
+  | TyRcd [(Label, SimpleType)]
